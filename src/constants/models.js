9396ac9f03050f71fe79e31ba541c40b6adefe99
hanhanhi <hanhanhi@users.noreply.github.com>
2018-11-19 17:15:17 +0800

    newOne

---
+++ src/constants/models.js
@@ -0,0 +1,7 @@
+const data = {message:[{id:'1',name:'瑞斯',message:'牛牛做不完'},
+{id:'2',name:'漢莉',message:'所以禮拜五吃什麼'},
+{id:'3',name:'魯斯',message:'我以後要騎摩托車上班，我車禍體質'},
+{id:'4',name:'峰哥',message:'我很失望，很失望，很失望'},
+{id:'5',name:'艾克',message:'我們是一個團隊'},]}
+
+export {data}
